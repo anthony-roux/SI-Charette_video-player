2803f469596d02797fe9ee79814731cb078bbbe7
anthony-roux <roux.anthony@hetic.net>
2020-04-10 20:59:27 +0200

    :alembic: test carousel with tiny-slide

--- src/js/smallSlider.js
+++ src/js/smallSlider.js
@@ -1,1 +1,43 @@
-console.log("caca");
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
+
+console.log('caca');
+
+let container = document.querySelector('.my-slider');
+
+var slider = tns({
+  container: '.my-slider',
+  mode: 'carousel', // or 'gallery'
+  axis: 'horizontal', // or 'vertical'
+  items: 4,
+  gutter: 1,
+  edgePadding: 100,
+  fixedWidth: false,
+  slideBy: 1,
+  controls: true,
+  controlsText: ['\u3008', '\u3009'],
+  controlsContainer: false,
+  nav: false,
+  navContainer: false,
+  navAsThumbnails: false,
+  arrowKeys: false,
+  speed: 300,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  autoplayDirection: 'forward',
+  autoplayText: ['start', 'stop'],
+  autoplayHoverPause: false,
+  autoplayButton: false,
+  autoplayButtonOutput: true,
+  autoplayResetOnVisibility: true,
+  loop: true,
+  rewind: false,
+  autoHeight: false,
+  responsive: false,
+  lazyload: false,
+  touch: true,
+  mouseDrag: true,
+  swipeAngle: 15,
+  nested: false,
+  freezable: true,
+  onInit: false
+});
