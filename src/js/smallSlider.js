803d2dad9aeaa0003e5064327e4bea74f0744ad4
anthony-roux <roux.anthony@hetic.net>
2020-04-11 00:23:10 +0200

    :lipstick: fixed margin, on carousel

--- src/js/smallSlider.js
+++ src/js/smallSlider.js
@@ -2,13 +2,53 @@ import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
 
 console.log('caca');
 
-let container = document.querySelector('.my-slider');
+let reprendre = document.querySelector('.my-slider');
 
 var slider = tns({
   container: '.my-slider',
   mode: 'carousel', // or 'gallery'
   axis: 'horizontal', // or 'vertical'
-  items: 4,
+  items: 5,
+  gutter: 1,
+  edgePadding: 0,
+  fixedWidth: false,
+  slideBy: 1,
+  controls: true,
+  controlsText: ['\u3008', '\u3009'],
+  controlsContainer: false,
+  nav: false,
+  navContainer: false,
+  navAsThumbnails: false,
+  arrowKeys: false,
+  speed: 400,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  autoplayDirection: 'forward',
+  autoplayText: ['start', 'stop'],
+  autoplayHoverPause: false,
+  autoplayButton: false,
+  autoplayButtonOutput: true,
+  autoplayResetOnVisibility: true,
+  loop: true,
+  rewind: false,
+  autoHeight: true,
+  responsive: false,
+  lazyload: false,
+  touch: true,
+  mouseDrag: true,
+  swipeAngle: 15,
+  nested: false,
+  freezable: true,
+  onInit: false
+});
+
+let populaire = document.querySelector('.my-slider--2');
+
+var slider = tns({
+  container: '.my-slider--2',
+  mode: 'carousel', // or 'gallery'
+  axis: 'horizontal', // or 'vertical'
+  items: 5,
   gutter: 1,
   edgePadding: 0,
   fixedWidth: false,
