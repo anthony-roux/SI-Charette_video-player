38f0337f18cea72e8129c67da9d5b28fa8f17a8c
anthony-roux <roux.anthony@hetic.net>
2020-04-11 23:35:37 +0200

    :sparkles: create medium sliders, and added img inside

--- src/js/mediumSlider.js
+++ src/js/mediumSlider.js
@@ -1,1 +1,89 @@
 console.log("pipi");
+
+import {
+  tns
+} from '../../node_modules/tiny-slider/src/tiny-slider';
+
+
+console.log('caca');
+
+let tendance = document.querySelector('.my-slider-medium-1');
+
+var slider = tns({
+  container: '.my-slider-medium-1',
+  mode: 'carousel', // or 'gallery'
+  axis: 'horizontal', // or 'vertical'
+  items: 5,
+  gutter: 1,
+  edgePadding: 0,
+  fixedWidth: false,
+  slideBy: 1,
+  controls: true,
+  controlsText: ['\u3008', '\u3009'],
+  controlsContainer: false,
+  nav: false,
+  navContainer: false,
+  navAsThumbnails: false,
+  arrowKeys: false,
+  speed: 400,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  autoplayDirection: 'forward',
+  autoplayText: ['start', 'stop'],
+  autoplayHoverPause: false,
+  autoplayButton: false,
+  autoplayButtonOutput: true,
+  autoplayResetOnVisibility: true,
+  loop: true,
+  rewind: false,
+  autoHeight: true,
+  responsive: false,
+  lazyload: false,
+  touch: true,
+  mouseDrag: true,
+  swipeAngle: 15,
+  nested: false,
+  freezable: true,
+  onInit: false
+});
+
+
+let discover = document.querySelector('.my-slider-medium-2');
+
+var slider = tns({
+  container: '.my-slider-medium-2',
+  mode: 'carousel', // or 'gallery'
+  axis: 'horizontal', // or 'vertical'
+  items: 5,
+  gutter: 1,
+  edgePadding: 0,
+  fixedWidth: false,
+  slideBy: 1,
+  controls: true,
+  controlsText: ['\u3008', '\u3009'],
+  controlsContainer: false,
+  nav: false,
+  navContainer: false,
+  navAsThumbnails: false,
+  arrowKeys: false,
+  speed: 400,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  autoplayDirection: 'forward',
+  autoplayText: ['start', 'stop'],
+  autoplayHoverPause: false,
+  autoplayButton: false,
+  autoplayButtonOutput: true,
+  autoplayResetOnVisibility: true,
+  loop: true,
+  rewind: false,
+  autoHeight: true,
+  responsive: false,
+  lazyload: false,
+  touch: true,
+  mouseDrag: true,
+  swipeAngle: 15,
+  nested: false,
+  freezable: true,
+  onInit: false
+});
