506f1c294e9a16be3ffc56059d32dc466260afe6
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-12 22:21:51 +0200

    add series pictures on page series

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,19 @@ buttonUsers.addEventListener('click', function () {
   buttonUsers.classList.toggle("is-open-down");
   menuDown.classList.toggle("menu-users-is-open");
 });
+
+fetch('http://localhost:8888/getCategoriesForUser.php?userId=3')
+.then(response => response.json())
+  .then(categories => {
+    
+    categories.forEach(category => {
+      console.log(category.anime);
+      category.anime.forEach(anime => {
+        console.log(anime.name);
+        console.log(anime.picture);
+      })
+    });
+  
+})
+.catch(error => console.error(error))
+ 
