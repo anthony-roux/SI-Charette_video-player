2555da02fe96761385fa2656a4ca542b3894bd8a
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-12 16:39:38 +0200

    update gitIgnore, add background to films card

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,14 @@
 /************* Genre *************/
 
-// var buttonGenre = document.querySelector(".genre__button");
-// var listGenre = document.querySelector(".genre__list");
+ var buttonGenre = document.querySelector(".genre__button");
+ var listGenre = document.querySelector(".genre__list");
 
-// buttonGenre.addEventListener('click', function () {
-//   console.log("coucou");
-//   buttonGenre.classList.toggle("is-open");
-//   listGenre.classList.toggle("is-open-genreList");
-//   // buttonGenre.style.display = "none";
-//   // listGenre.style.display = "flex";
-// });
+ buttonGenre.addEventListener('click', function () {
+  console.log("coucou");
+  buttonGenre.classList.toggle("is-open");
+  listGenre.classList.toggle("is-open-genreList");
+
+});
 
 
 
