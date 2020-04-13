40526ab8b3e4718bc3621348221c007078fdd889
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-13 22:14:49 +0200

    add users on js and change position of video on pages

--- src/js/connection.js
+++ src/js/connection.js
@@ -3,22 +3,48 @@ var emailInput = document.querySelector("#email");
 var passwordInput = document.querySelector("#password");
 
 
-var user = {
+var user = [{
   id: "3",
   email: "vivian.priska@gmail.com",
   password: "kevingila",
-}
+  },
+  {
+  id: "4",
+  email: "oum.caramel@gmail.com",
+  password: "oumoum",
+  },
+  {
+    id: "3",
+    email: "quentinsan@gmail.com",
+    password: "quentin",
+  },
+  {
+    id: "3",
+    email: "hannachan@gmail.com",
+    password: "hannah",
+  }
+]
 
 buttonConnection.addEventListener("click", function (e) {
   e.preventDefault();
   var email = emailInput.value;
   var password = passwordInput.value;
 
-  if (email === user.email && password === user.password) {
+  if (email === user[0].email && password === user[0].password) {
     
     window.localStorage.setItem('userId', user.id);
     window.location.href = "index.html";
-  } else {
+  } else if(email === user[1].email && password === user[1].password){
+    window.localStorage.setItem('userId', user.id);
+    window.location.href = "index.html";
+  } else if(email === user[2].email && password === user[2].password){
+    window.localStorage.setItem('userId', user.id);
+    window.location.href = "index.html";
+   }
+  else if(email === user[3].email && password === user[3].password){
+    window.localStorage.setItem('userId', user.id);
+    window.location.href = "index.html";}
+  else {
     alert("go awway");
   };
 })
