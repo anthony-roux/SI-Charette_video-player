403a3f7dda4da9a4ab03504e0b656222fef8c9b5
anthony-roux <roux.anthony@hetic.net>
2020-04-14 00:32:59 +0200

    :bug: fix bug

--- src/js/connection.js
+++ src/js/connection.js
@@ -4,14 +4,14 @@ var passwordInput = document.querySelector("#password");
 
 
 var user = [{
-  id: "3",
-  email: "vivian.priska@gmail.com",
-  password: "kevingila",
+    id: "3",
+    email: "vivian.priska@gmail.com",
+    password: "kevingila",
   },
   {
-  id: "4",
-  email: "oum.caramel@gmail.com",
-  password: "oumoum",
+    id: "4",
+    email: "oum.caramel@gmail.com",
+    password: "oumoum",
   },
   {
     id: "3",
@@ -29,22 +29,21 @@ buttonConnection.addEventListener("click", function (e) {
   e.preventDefault();
   var email = emailInput.value;
   var password = passwordInput.value;
-
+  debugger;
   if (email === user[0].email && password === user[0].password) {
-    
+
     window.localStorage.setItem('userId', user.id);
     window.location.href = "index.html";
-  } else if(email === user[1].email && password === user[1].password){
+  } else if (email === user[1].email && password === user[1].password) {
     window.localStorage.setItem('userId', user.id);
     window.location.href = "index.html";
-  } else if(email === user[2].email && password === user[2].password){
+  } else if (email === user[2].email && password === user[2].password) {
     window.localStorage.setItem('userId', user.id);
     window.location.href = "index.html";
-   }
-  else if(email === user[3].email && password === user[3].password){
+  } else if (email === user[3].email && password === user[3].password) {
     window.localStorage.setItem('userId', user.id);
-    window.location.href = "index.html";}
-  else {
+    window.location.href = "index.html";
+  } else {
     alert("go awway");
   };
 })
--- src/js/script.js
+++ src/js/script.js
@@ -4,99 +4,99 @@ import iconPlay from '/icons/icons-play-vignette-hover.svg';
 
 /************* users menu *************/
 
-let buttonUsers = document.querySelector(".headerSecondPart__profilArrow");
-let menuDown = document.querySelector(".menuUsers");
+let buttonUsers = document.querySelector('.headerSecondPart__profilArrow');
+let menuDown = document.querySelector('.menuUsers');
 console.log(menuDown);
 console.log(buttonUsers);
-buttonUsers.addEventListener('click', function () {
-  console.log("mama");
-  buttonUsers.classList.toggle("is-open-down");
-  menuDown.classList.toggle("menu-users-is-open");
-
+buttonUsers.addEventListener('click', function() {
+  console.log('mama');
+  buttonUsers.classList.toggle('is-open-down');
+  menuDown.classList.toggle('menu-users-is-open');
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  var userId = window.localStorage.getItem("userId");
-  var userLogOut = document.querySelector(".userMenuSecondPart__logOut");
-  var reprendreSlider = document.querySelector(".reprendre");
-  
+document.addEventListener('DOMContentLoaded', () => {
+  var userId = window.localStorage.getItem('userId');
+  var userLogOut = document.querySelector('.userMenuSecondPart__logOut');
+  var reprendreSlider = document.querySelector('.reprendre');
+
   // route to connexion if not logged
-  if (userId == null || userId == undefined || userId == "null") {
-    window.location.href = "connexion.html";
+  if (userId == null || userId == undefined || userId == 'null') {
+    window.location.href = 'connexion.html';
   }
 
-  fetch('http://localhost:8888/getCategoriesForUser.php?userId='+userId)
-  .then(response => response.json())
+  fetch('http://localhost:8888/getCategoriesForUser.php?userId=' + userId)
+    .then(response => response.json())
     .then(categories => {
-      
       categories.forEach(category => {
         console.log(category);
-        switch(category.name) {
-          case "reprendre":
+        switch (category.name) {
+          case 'reprendre':
             console.log(category.anime);
             category.anime.forEach(anime => {
               console.log(anime.name);
               console.log(anime.picture);
               //reprendreSlider.insertAdjacentHTML('beforeend', generateSliderElement(anime.picture, anime.name));
-            })
+            });
             break;
-          case "ma liste":
+          case 'ma liste':
             // todo
             break;
           default:
             break;
         }
-        
       });
 
       //window.reprendreSlider.rebuild();
-    
-  })
-  .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error));
 
   // logout
-  userLogOut.addEventListener("click", () =>{
+  userLogOut.addEventListener('click', () => {
     window.localStorage.setItem('userId', null);
-    window.location.href = "connexion.html";
+    window.location.href = 'connexion.html';
   });
 
   console.log(reprendreSlider);
-
-  
-  
-
-
-
 });
 
 var obj = {
-  "picture": "",
-  "title": ""
-}
+  picture: '',
+  title: ''
+};
 
 function generateSliderElement(picture, title) {
-  return "<div class='slide slideCard'>" +
-  "<img class='over img' src='"+picture+"' alt='' />" +
-  "<div class='slideCard__info'>" +
-    "<img class='slideCard__add' src='"+iconAdd+"' alt='add' />" +
-    "<img class='slideCard__star' src='"+iconStar+"' alt='star' />" +
+  return (
+    "<div class='slide slideCard'>" +
+    "<img class='over img' src='" +
+    picture +
+    "' alt='' />" +
+    "<div class='slideCard__info'>" +
+    "<img class='slideCard__add' src='" +
+    iconAdd +
+    "' alt='add' />" +
+    "<img class='slideCard__star' src='" +
+    iconStar +
+    "' alt='star' />" +
     "<div class='slideCard__lecture'>" +
-      "<img class='slideCard__button' src='"+iconPlay+"' alt='' />" +
-      "<p class='play-hover'>lecture</p>" +
-    "</div>" +
+    "<img class='slideCard__button' src='" +
+    iconPlay +
+    "' alt='' />" +
+    "<p class='play-hover'>lecture</p>" +
+    '</div>' +
     "<h1 class='slideCard__title title-cards-hover'>" +
     title +
-    "</h1>" +
+    '</h1>' +
     "<p class='slideCard__synopsis synopsis-cards-hover'>" +
-      "Jeune lycéen, Hodaka fuit son île pour rejoindre Tokyo." +
-      "<br />Sans argent ni emploi, il tente de survivre dans la" +
-      "jungle urbaine." +
-    "</p>" +
+    'Jeune lycéen, Hodaka fuit son île pour rejoindre Tokyo.' +
+    '<br />Sans argent ni emploi, il tente de survivre dans la' +
+    'jungle urbaine.' +
+    '</p>' +
     "<div class='slideCard__season suggestion__season'>" +
-      "<p class='time-date'>2019</p>" +
-      "<p class='time-date'>saison 3</p>"
-      "<img class='slideCard__average' src='/icons/icon_stars.svg' alt='note des utilisateurs' />" +
-    "</div>" +
-    "</div>" +
-    "</div>";
+    "<p class='time-date'>2019</p>" +
+    "<p class='time-date'>saison 3</p>"
+  );
+  "<img class='slideCard__average' src='/icons/icon_stars.svg' alt='note des utilisateurs' />" +
+    '</div>' +
+    '</div>' +
+    '</div>';
 }
