1f356cfb14bbe15e1b3af6532ecdfd99d7d08642
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-13 12:16:33 +0200

    separating file js ‘genre’ and add to page film and page series

---
+++ src/js/buttonGenre.js
@@ -0,0 +1,11 @@
+/************* Genre *************/
+
+var buttonGenre = document.querySelector(".genre__button");
+var listGenre = document.querySelector(".genre__list");
+var Genre = document.querySelector(".genre");
+
+buttonGenre.addEventListener('click', function () {
+ console.log("coucou");
+ buttonGenre.classList.toggle("is-open");
+  listGenre.classList.toggle("is-open-genreList");  
+});
