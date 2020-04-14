6f2144933658ca0ba54b143406d450c5e32ff736
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-15 00:37:38 +0200

    fix user session

--- src/js/userpage.js
+++ src/js/userpage.js
@@ -3,20 +3,28 @@ import { users } from './user.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-var profilPicture = document.querySelector('profil__picture');
+var profilPicture = document.querySelector('.profil__picture');
+var profilName = document.querySelector('.profil__name');
 var totalAnime = document.querySelector('.animes__info--inProgress p');
 var newAnime = document.querySelector('.animes__info--new p');
 
 var userhanna = document.querySelector('.userHanna');
 var userquentin = document.querySelector('.userQuentin');
 var userPriska = document.querySelector('.userPriska');
-  var userOum = document.querySelector('.userOum');
+var userOum = document.querySelector('.userOum');
+var pictureUserOnMenu = document.querySelector('.headerSecondPart__profilPicture');
+  var recentUser = document.querySelector('.menuUsers__fonts');
   
   var userId = window.localStorage.getItem('userId');
 
   var user = users.find(function (user) {
     return user.id === userId;
   });
+
+  profilPicture.src = user.picture;
+  profilName.innerHTML = user.name;
+  pictureUserOnMenu.src = user.picture;
+  recentUser.innerHTML = user.name;
 
   console.log(user.id);
   if (user.id === "3") {
