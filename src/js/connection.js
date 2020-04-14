85d39cebce4c6f265f74f7dec108b4c40a5819be
anthony-roux <roux.anthony@hetic.net>
2020-04-14 23:52:06 +0200

    lot a change with k

--- src/js/connection.js
+++ src/js/connection.js
@@ -1,4 +1,4 @@
-import {users} from './user.js';
+import { users } from './user.js';
 
 var buttonConnection = document.querySelector('.inputInfo__btn');
 var emailInput = document.querySelector('#email');
@@ -10,24 +10,23 @@ buttonConnection.addEventListener('click', function(e) {
   var password = passwordInput.value;
   if (email === users[0].email && password === users[0].password) {
     window.localStorage.setItem('userId', users[0].id);
-    window.location.href = 'index.html';
+    window.location.href = 'homepage.html';
   } else if (email === users[1].email && password === users[1].password) {
     window.localStorage.setItem('userId', users[1].id);
-    window.location.href = 'index.html';
+    window.location.href = 'homepage.html';
   } else if (email === users[2].email && password === users[2].password) {
     window.localStorage.setItem('userId', users[2].id);
-    window.location.href = 'index.html';
+    window.location.href = 'homepage.html';
   } else if (email === users[3].email && password === users[3].password) {
     window.localStorage.setItem('userId', users[3].id);
-    window.location.href = 'index.html';
+    window.location.href = 'homepage.html';
   } else if (email === users[4].email && password === users[4].password) {
     window.localStorage.setItem('userId', users[4].id);
-    window.location.href = 'index.html';
+    window.location.href = 'homepage.html';
   } else if (email === users[5].email && password === users[5].password) {
     window.localStorage.setItem('userId', users[5].id);
-    window.location.href = 'index.html';
-  }
-    else {
+    window.location.href = 'homepage.html';
+  } else {
     alert('go awway');
   }
 });
