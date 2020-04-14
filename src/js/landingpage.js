421254728670f743c79c9c6bd7a14acb10d3552c
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-04-14 14:47:14 +0200

    change style button genre and adding landing page

---
+++ src/js/landingpage.js
@@ -0,0 +1,5 @@
+var button_identification = document.querySelector(".landingHero__header__layoutbtn__btnLogin");
+
+button_identification.addEventListener('click', function () {
+  window.location.href = 'connexion.html';
+});
